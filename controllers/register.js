bee57e18974b87894d2597ef528556706b5d53ed
itay <itaym99@gmail.com>
2025-01-13 17:36:56 +0200

    added cookies for user info

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,7 @@
 const Users = require('../models/users');
+const Cookies = require('cookies');
+const keys = ['keyboard cat'];
+
 
 exports.handleUserRegistration = (req, res) => {
     const {email, firstName, lastName, password} = req.body;
@@ -22,11 +25,43 @@ exports.handleUserRegistration = (req, res) => {
 };
 
 exports.getRegisterPage = (req, res) => {
-    res.render('register', { title: 'Register Page' });
+    const cookies = new Cookies(req, res, { keys: keys });
+    let userInfo = cookies.get('userInfo');
+
+    console.log('User Info from cookies:', userInfo);
+    if (!userInfo) {
+        res.render('register', { title: 'Register Page' });
+    }
+    else {
+        // Parse the JSON string from the cookie
+        userInfo = JSON.parse(userInfo);
+        res.render('register', {
+            title: 'Register Page',
+            firstName: userInfo.firstName,
+            lastName: userInfo.lastName,
+            email: userInfo.email
+        });
+    }
 };
 
 exports.getPasswordPage = (req, res) => {
     const { email, firstName, lastName } = req.query;
+    // create cookie
+
+    const cookies = new Cookies(req, res, { keys: keys });
+    cookies.set('userInfo', JSON.stringify({
+            email: email,
+            firstName: firstName,
+            lastName: lastName
+        }),
+        {
+            path: '/',      // Cookie is available site-wide
+            maxAge: 30000   // Cookie lasts for 30 seconds (in milliseconds)
+        }
+    );
+
+
+
     res.render('register-password', { title: 'Password Page',
         email: email,
         firstName: firstName,
